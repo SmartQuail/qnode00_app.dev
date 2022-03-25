f6a1d4e63c3bfa56c8859ef3d2850b950911862a
SmartQuail <smartquail.info@gmail.com>
2022-03-25 03:35:26 -0500

    Update settings.py

--- qnode00_app/qnode00_app/settings.py
+++ qnode00_app/qnode00_app/settings.py
@@ -137,3 +137,7 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*'
+}
