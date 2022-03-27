fbdfa1d6c0d513bbfa8e0b4a264a657e05bcd95e
SmartQuail <smartquail.info@gmail.com>
2022-03-27 01:08:59 -0500

    Update settings.py

--- qnode00_app/qnode00_app/settings.py
+++ qnode00_app/qnode00_app/settings.py
@@ -84,12 +84,11 @@ WSGI_APPLICATION = 'qnode00_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        
     }
 }
 
