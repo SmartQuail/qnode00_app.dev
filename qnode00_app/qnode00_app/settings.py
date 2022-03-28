b3fa3a10890ef66a0643617b1a6a666d7b98de10
SmartQuail <smartquail.info@gmail.com>
2022-03-28 11:18:11 -0500

    Update settings.py
    Cambios en el puerto

--- qnode00_app/qnode00_app/settings.py
+++ qnode00_app/qnode00_app/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
