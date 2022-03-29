61cc9121568c8232aa2590bd58afb26ebf6d744c
SmartQuail <smartquail.info@gmail.com>
2022-03-29 04:04:19 -0500

    cambios

--- qnode00_app/manage.py
+++ qnode00_app/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
+
 
 
 def main():
@@ -20,5 +20,5 @@ def main():
 
 
 if __name__ == '__main__':
-    dotenv.read_dotenv()
+   
     main()
